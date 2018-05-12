cba69154ef6ad078d88f05c335aff8f22b5c8726
Coursal <coursal@espiv.net>
2018-05-12 22:51:57 +0000

    added source files

---
+++ client.c
@@ -0,0 +1,107 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+
+int main(int argc, char** argv)
+{
+	int server_socket;
+	int portno;
+	
+	int index=0;
+	int i;
+	
+	int *X;
+	int n;
+	float average;
+	
+	struct sockaddr_in serv_addr;
+	
+	struct hostent *server;
+	
+	char buffer[256];
+	
+	portno=atoi(argv[2]);
+	
+	server_socket=socket(AF_INET,SOCK_STREAM,0);
+	
+	server=gethostbyname(argv[1]);
+	
+	
+	bzero((char *) &serv_addr, sizeof(serv_addr));
+	serv_addr.sin_family=AF_INET;
+	bcopy((char *) server->h_addr, (char *) &serv_addr.sin_addr.s_addr, server->h_length);
+	serv_addr.sin_port=htons(portno);
+	
+	connect(server_socket, (struct sockaddr *) &serv_addr, sizeof(serv_addr));
+	
+	////////////////////////////////////////////////////////
+	//sending the number of elements (n)
+	////////////////////////////////////////////////////////
+	printf("Give n to the server: ");
+	bzero(buffer, 256);
+	fgets(buffer, 255, stdin);
+	n=atoi(buffer);
+	
+	write(server_socket,buffer,strlen(buffer));
+	////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////
+
+	////////////////////////////////////////////////////////
+	//initializing and sending the array
+	////////////////////////////////////////////////////////
+	X=(int *)malloc(n*sizeof(int));
+
+	for(i=0;i<n;i++)
+	{
+		printf("X[%d] = ", i);
+		scanf("%d", &X[i]);
+	}
+	
+	bzero(buffer,256);
+	for(i=0;i<n;i++)
+		index+=sprintf(&buffer[index], "%d#", X[i]);
+
+	write(server_socket,buffer,strlen(buffer));
+	////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////
+	
+	
+	////////////////////////////////////////////////////////
+	//receiving average...
+	////////////////////////////////////////////////////////
+	bzero(buffer,256);
+	read(server_socket,buffer,255);
+	average=atof(buffer);
+	printf("Average: %.2f \n", average);
+	////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////
+	
+	
+	////////////////////////////////////////////////////////
+	//receiving max and min elements...
+	////////////////////////////////////////////////////////
+	int max_and_min[2];
+	
+	bzero(buffer,256);
+	read(server_socket,buffer,255);
+	
+	i=0;
+	
+	char *p=strtok(buffer,"#");
+	while(p!=NULL)
+	{
+		max_and_min[i++]=atoi(p);
+		p=strtok(NULL,"#");	
+	}
+
+	printf("Max element: %d \nMin element: %d \n", max_and_min[0], max_and_min[1]);
+	////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////
+	
+
+	return 0;
+}
---
+++ server.c
@@ -0,0 +1,129 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+void *session_function(void *arg)
+{
+	int client=(int) arg;
+	int n;
+	int *X;
+	int i=0;
+	int sum=0;
+	float average;
+	int max;
+	int min;
+	
+	char data[100];
+
+	printf("Connected.\n");
+
+	////////////////////////////////////////////////////////
+	//receiving the number of elements (n)...
+	////////////////////////////////////////////////////////
+	recv(client,data,100,0);
+	n=atoi(data);
+	////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////
+	
+	X=(int *)malloc(n*sizeof(int));
+	
+	////////////////////////////////////////////////////////
+	//calculating and sending average...
+	////////////////////////////////////////////////////////
+	bzero(data,100);
+	recv(client,data,100,0);
+	
+	char *p=strtok(data,"#");
+	while(p!=NULL)
+	{
+		X[i++]=atoi(p);
+		p=strtok(NULL,"#");
+	}
+
+	for(i=0;i<n;i++)
+		sum+=X[i];
+
+	average=(float) sum/n;
+	bzero(data,100);
+	sprintf(data,"%f", average);
+	
+	send(client,data,sizeof(data),0);
+	////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////
+	
+	
+	////////////////////////////////////////////////////////
+	//finding and sending max and min elements...
+	////////////////////////////////////////////////////////
+	max=X[0];
+	min=X[0];
+	
+	for(i=0;i<n;i++)
+	{
+		if(X[i]>max)
+			max=X[i];
+	}
+
+	for(i=0;i<n;i++)
+	{
+		if(X[i]<min)
+			min=X[i];
+	}
+	
+	//printf("Max element: %d \n Min element: %d \n", max, min);
+	bzero(data,100);
+	sprintf(data,"%d#%d", max, min);
+	printf("Data to send: \"%s\"  \n", data);
+
+	sleep(1);
+	send(client,data,sizeof(data),0);
+	////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////
+	
+	close(client);
+	pthread_exit(NULL);
+}
+
+
+int main(int argc, char** argv)
+{
+	int input_socket;
+	int accepted_socket;
+	int portno;
+	int clilen;
+	int i=0;
+	
+	struct sockaddr_in serv_addr;
+	struct sockaddr_in cli_addr;
+	
+	pthread_t thread[50];
+
+	input_socket = socket(AF_INET, SOCK_STREAM, 0);
+
+	bzero((char *) &serv_addr, sizeof(serv_addr));
+	
+	portno = atoi(argv[1]);
+	
+	serv_addr.sin_family=AF_INET;
+	serv_addr.sin_port=htons(portno);
+	serv_addr.sin_addr.s_addr=INADDR_ANY;
+	
+	bind(input_socket, (struct sockaddr *) &serv_addr, sizeof(serv_addr));
+
+	listen(input_socket,5);
+
+	while(1)
+	{
+		printf("Waiting for connection...\n");
+		clilen=sizeof(cli_addr);
+		accepted_socket=accept(input_socket, (struct sockaddr *) &cli_addr, &clilen);
+	
+		pthread_create(&(thread[i++]), NULL, session_function, (void *) accepted_socket);
+	}
+
+
+	return 0;
+}
